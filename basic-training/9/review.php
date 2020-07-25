161fdfbfe6551d1595a62857c2841266ea5eec6a
Rixxan <rixxan@hullseals.space>
2020-07-25 14:46:13 -0400

    9 Review Complete

--- basic-training/9/review.php
+++ basic-training/9/review.php
@@ -20,7 +20,6 @@ require '../../assets/nextSetter.php';
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <?php include '../../assets/headerCenter.php'; ?>
     <meta content="Welcome to the Hull Seals, Elite Dangerous's Premier Hull Repair Specialists!" name="description">
@@ -31,6 +30,20 @@ require '../../assets/nextSetter.php';
       <?php include '../../assets/menuCode.php';?>
         <section class="introduction container">
 	    <article id="intro3">
+        <h2>Congrats, CMDR!</h2>
+        <p>Congrats on completing this section of your Seal Training. <br><br> This course represents one half of your training to become qualified as one of Elite's most dedicated responders: The Seals.<br><br>
+          After completing this course, you'll need to undertake a drill, ran by our trainers, in order to make sure that you have all the skills and knowledge required in order to best serve our clients.<br><br>
+          In order to progress, you'll need to sign up for a drill and follow the instructions in the video you just watched. Here are some of the links you'll need. It's strongly advised you record these for later.
+        </p>
+        <div class="list-group" style="max-width: 60%; text-align:center;">
+          <a href="https://hullseals.space/knowledge/books/how-to-join/page/how-to-join-the-hull-seals" target="_blank" class="list-group-item list-group-item-action list-group-item-dark">"How to Join the Seals" Wiki Page</a>
+          <a href="https://hullseals.space/SuperSecretInviteLink" target="_blank" class="list-group-item list-group-item-action list-group-item-dark">Our Discord</a>
+          <a href="http://hullse.al/tSignUp" target="_blank" class="list-group-item list-group-item-action list-group-item-dark">Drill Signup Form</a>
+          <a href="https://hullse.al/SOP" target="_blank" class="list-group-item list-group-item-action list-group-item-dark">Seal SOP</a>
+        </div>
+        <br>
+        <p><a href=".." class="btn btn-small btn-danger" style="float: right;">Done? Go back to Basic Training Home</a></p>
+        <br>
       </article>
             <div class="clearfix"></div>
         </section>
