3913e61481e7eced6b0235888262d8a98eba5989
David Sangrey <davidsangrey@gmail.com>
2020-09-04 18:15:57 +0000

    Update fetch.php

--- training-paperwork/fetch.php
+++ training-paperwork/fetch.php
@@ -5,7 +5,7 @@ require_once '../../users/init.php';
 $db = include '../assets/db.php';
 $mysqli = new mysqli($db['server'], $db['user'], $db['pass'], "sealsudb", $db['port']);
 	$sql = "SELECT seal_name, seal_id FROM staff
-			WHERE (seal_name LIKE '%".$_GET['query']."%') AND (seal_id <> 0)
+			WHERE (seal_name LIKE '%".$_GET['query']."%') AND (seal_id <> 0) AND del_flag <> 1
       ORDER BY seal_name ASC
 			LIMIT 10";
 
