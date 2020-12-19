7caa280da536e6dbfad823c3417cd4e48aa0aadf
Rixxan <rixxan@hullseals.space>
2020-12-19 00:20:56 -0500

    Restart scheduling form
    Much more database-driven, and I've learned a lot since I first wrote this. Will use Modals to mak everything interactive (God I love those things). Prereq for scheduling.

--- scheduling/index.php
+++ scheduling/index.php
@@ -2,69 +2,99 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+//UserSpice Required
 require_once '../../users/init.php';  //make sure this path is correct!
 if (!securePage($_SERVER['PHP_SELF'])){die();}
 
-//DB Connection Info
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$db = include '../assets/db.php';
-$mysqli = new mysqli($db['server'], $db['user'], $db['pass'], $db['db'], $db['port']);
+//IP Tracking Stuff
+require '../../assets/includes/ipinfo.php';
 
+$db = include '../assets/db.php';
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$mysqli = new mysqli($db['server'], $db['user'], $db['pass'], 'training_records', $db['port']);
+$platformList = [];
+$res = $mysqli->query('SELECT * FROM lookups.platform_lu ORDER BY platform_id');
+while ($burgerking = $res->fetch_assoc())
+{
+    $platformList[$burgerking['platform_id']] = $burgerking['platform_name'];
+}
 ?>
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <?php include '../../assets/includes/headerCenter.php'; ?>
-    <title>Lesson Scheduling | The Hull Seals</title>
-    <meta content="Lesson Scheduling System" name="description">
-</head>
-<body>
+  <html lang="en">
+  <head>
+      <meta content="Trainee Signup Form" name="description">
+      <title>Trainee Signup Form | The Hull Seals</title>
+      <?php include '../../assets/includes/headerCenter.php'; ?>
+  </head>
+  <body>
     <div id="home">
       <?php include '../../assets/includes/menuCode.php';?>
-        <section class="introduction container">
-	    <article id="intro3">
-        <h1>Welcome, Pup!</h1>
-        <p>Here you can view or edit your availability for lessons.</p>
-        <br>
-        <table class="table table-dark table-striped table-bordered table-hover table-responsive-md">
-          <thead>
-          <tr>
-              <th>Module</th>
-              <th>Status</th>
-              <th>Length</th>
-              <th>Options</th>
-          </tr>
-        </thead>
-        <tbody>
+      <section class="introduction container">
+        <article id="intro3">
+          <h1>Pup Signup Form</h1>
+          <p>
+            Welcome, CMDR. You can request to be scheduled for Seal trainings here.<br>
+            Remember, you may be asked to serve as a stand-in client for another pup. All training is held on the Hull Seals IRC server in the #drill-channel.
+            <br><br>
+            You will receive an email when your drills are scheduled!
+          </p>
           <?php
-          //Need DBinfo
-          //$stmt = $mysqli->prepare("");
-          //$stmt->bind_param("i", $user->data()->id);
-          //$stmt->execute();
-          //$result = $stmt->get_result();
-
-          //while ($row = $result->fetch_assoc()) {
-            //$field1name = $row["day"];
-            //$field2name = $row["time"];
-            //$field3name = $row["location"];
-            //$field4name = $row["schedule_ID"];
-            //echo '<tr>
-            //  <td>'.$field2name.'</td>
-            //  <td>'.$field1name.'</td>
-            //  <td>'.$field3name.'</td>
-            //  <td><a href="edit-availability.php?cne='.$field4name.'" class="btn btn-warning active">Edit</a></td>
-            //  <td><a href="rem-irc.php?cne='.$field4name.'" class="btn btn-danger active">Delete</a></td>
-            //</tr>';
-          //}
-          //$result->free();
+            $stmt = $mysqli->prepare("SELECT sch_ID, platform_name, training_description, sch_max, loc_description
+            FROM training.schedule_requests
+            INNER JOIN lookups.platform_lu ON seal_PLT = platform_id
+            INNER JOIN lookups.training_lu ON sch_type = training_id
+            INNER JOIN lookups.traininglocation_lu ON seal_LOC = loc_ID
+            WHERE seal_ID = ?");
+				    $stmt->bind_param("i", $user->data()->id);
+				    $stmt->execute();
+            $result = $stmt->get_result();
+				    if($result->num_rows === 0) {
+              $norows = 1;
+              }
+            else {
+              $norows = 0;
+            echo '<em>Here is your current training request:</em>
+            <table class="table table-dark table-striped table-bordered table-hover table-responsive-md">
+              <tr>
+                <td>Type</td>
+                <td>Platform</td>
+                <td>Location</td>
+                <td>Time Blocks</td>
+                <td>Days of the Week</td>
+                <td>Max Lessions per Week</td>
+                <td colspan="2">Options</td>
+              </tr>';
+              while ($row = $result->fetch_assoc()) {
+				            $field1name = $row['sch_ID'];
+				            $field2name = $row["training_description"];
+				            $field3name = $row["platform_name"];
+                    $field4name = $row["sch_max"];
+                    $field5name = $row["loc_description"];
+              echo '<tr>
+                <td>'.$field2name.'</td>
+                <td>'.$field3name.'</td>
+                <td>'.$field5name.'</td>
+                <td></td>
+                <td></td>
+                <td>'.$field4name.'</td>
+                <td><button type="button" class="btn btn-warning active" data-toggle="modal" data-target="#moE'.$field1name.'">Edit</button></td>
+				        <td><button type="button" class="btn btn-danger active" data-toggle="modal" data-target="#mo'.$field1name.'">Delete</button></td>';
+              }
+            }
+            echo '</table>';
+            if($norows === 1) {
+              echo '<p> You may only have one training request at a time.</p>';
+            }
+            else {
+              echo '<button class="btn btn-success btn-lg active" data-target="#moNew" data-toggle="modal" type="button">New Training Request</button>';
+            }
+            $result->free();
           ?>
-        </tbody>
-        </table>
-        <p><a href="add-schedule.php" class="btn btn-success btn-lg active">New Scheduling Availability</a><a href=".." class="btn btn-small btn-danger" style="float: right;">Go To Training Index</a></p>
-      </article>
-            <div class="clearfix"></div>
-        </section>
+        </article>
+        <div class="clearfix"></div>
+      </section>
     </div>
     <?php include '../../assets/includes/footer.php'; ?>
-</body>
+  </body>
 </html>
